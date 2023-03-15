4f355b4064ec85cf7e8d9e475b6df3e8a0192255
Conner Swenberg <cls364@cornell.edu>
2023-03-15 04:21:34 -0400

    Refactor event handler on new contract ABI and db schema

--- .spec/handlers/index.js
+++ .spec/handlers/index.js
@@ -1,26 +1,32 @@
 /**
  * Mark Actions as successful upon receiving the "Execute" contract event.
  */
-async function onExecuteEvent(event, db, logger) {
-    const chainId = Number(event.origin.chainId)
-    const { actionDigest } = event.data
+async function onSuccessEvent(event, db, logger) {
+  const { actionHash } = event.data;
 
-    // Update Action record.
-    const numRecordsUpdated = await db.table('Action')
-        .update({ status: 'SUCCESS' })
-        .where({ chainId, actionDigest })
-    
-    // Log result.
-    numRecordsUpdated
-        ? logger.info(`Updated ${numRecordsUpdated} Action record${numRecordsUpdated > 1 ? 's' : ''}.`)
-        : logger.error(`No Action record found for (chainId=${chainId}, actionDigest=${actionDigest}).`)
+  // Update Action record.
+  // Actions unique by actionHash, which is a EIP712 hash deriving from
+  // chainId, safe address, nonce, call parameters, version, and verifying contract
+  const numRecordsUpdated = await db
+    .table("Action")
+    .update({ status: "SUCCESS" })
+    .where({ actionHash });
+
+  // Log result.
+  numRecordsUpdated
+    ? logger.info(
+        `Updated ${numRecordsUpdated} Action record${
+          numRecordsUpdated > 1 ? "s" : ""
+        }.`
+      )
+    : logger.error(`No Action record found for (actionHash=${actionHash}).`);
 }
 
 /**
  * Tap into any Spec event.
  */
 const eventHandlers = {
-    'contracts.station.ParallelProcessor.Execute': onExecuteEvent,
-}
+  "contracts.station.ParallelProcessor.Success": onSuccessEvent,
+};
 
-module.exports = eventHandlers
+module.exports = eventHandlers;
