86f3350df4df82797f8793c2b5bb3e7898731d70
Michael Gingras <mcg79@cornell.edu>
2023-07-12 11:00:05 -0600

    Add token operator

---
+++ TokenOperator/spec.ts
@@ -0,0 +1,41 @@
+import {
+  LiveObject,
+  Spec,
+  Property,
+  Event,
+  OnEvent,
+  Address,
+  BigInt,
+  String,
+} from "@spec.dev/core";
+
+/**
+ * A TokenOperator on Station.
+ */
+@Spec({
+  uniqueBy: ["contractAddress", "operatorAddress", "chainId"],
+})
+class TokenOperator extends LiveObject {
+  // The membership contract.
+  @Property()
+  contractAddress: Address;
+
+  // The token operator address.
+  @Property()
+  operatorAddress: Address;
+
+  // The permissions of the token operator.
+  @Property()
+  permissions: String;
+
+  // ==== Event Handlers ===================
+
+  @OnEvent("station.Membership.Permit")
+  onTransfer(event: Event) {
+    this.contractAddress = event.origin.contractAddress;
+    this.operatorAddress = event.data.account;
+    this.permissions = event.data.permissions;
+  }
+}
+
+export default TokenOperator;
