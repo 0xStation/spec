dd84deeacb64235b935ad7c0fbbfcbf7504ecafd
Michael Gingras <mcg79@cornell.edu>
2023-07-18 10:29:36 -0600

    Removing String from Spec, adding MembershipFactory namespace

--- Membership/spec.ts
+++ Membership/spec.ts
@@ -1,49 +1,60 @@
-import { LiveObject, Spec, Property, Event, OnEvent, Address, BigInt, Timestamp, ZERO_ADDRESS } from '@spec.dev/core'
+import {
+  LiveObject,
+  Spec,
+  Property,
+  Event,
+  OnEvent,
+  Address,
+  BigInt,
+  Timestamp,
+  ZERO_ADDRESS,
+} from "@spec.dev/core";
 
 /**
  * A Membership NFT on Station.
  */
 @Spec({
-    uniqueBy: ['contractAddress', 'tokenId', 'chainId'] 
+  uniqueBy: ["contractAddress", "tokenId", "chainId"],
 })
 class Membership extends LiveObject {
-    // The membership contract.
-    @Property()
-    contractAddress: Address
+  // The membership contract.
+  @Property()
+  contractAddress: Address;
 
-    // The NFT token id.
-    @Property()
-    tokenId: BigInt
+  // The NFT token id.
+  @Property()
+  tokenId: BigInt;
 
-    // The current NFT owner.
-    @Property()
-    ownerAddress: Address
+  // The current NFT owner.
+  @Property()
+  ownerAddress: Address;
 
-    // The token-bound account.
-    @Property()
-    tbaAddress: Address
+  // The token-bound account.
+  @Property()
+  tbaAddress: Address;
 
-    // When the membership was minted.
-    @Property({ canUpdate: false })
-    joinedAt: Timestamp
+  // When the membership was minted.
+  @Property({ canUpdate: false })
+  joinedAt: Timestamp;
 
-    // ==== Event Handlers ===================
+  // ==== Event Handlers ===================
 
-    @OnEvent('station.Membership.Transfer')
-    onTransfer(event: Event) {
-        this.contractAddress = event.origin.contractAddress
-        this.tokenId = BigInt.from(event.data.tokenId)
-        this.ownerAddress = event.data.to
-        this.tbaAddress = null // v2
-        if (event.data.from === ZERO_ADDRESS) { // mint
-            this.joinedAt = this.blockTimestamp
-        }
+  @OnEvent("station.Membership.Transfer")
+  onTransfer(event: Event) {
+    this.contractAddress = event.origin.contractAddress;
+    this.tokenId = BigInt.from(event.data.tokenId);
+    this.ownerAddress = event.data.to;
+    this.tbaAddress = null; // v2
+    if (event.data.from === ZERO_ADDRESS) {
+      // mint
+      this.joinedAt = this.blockTimestamp;
     }
+  }
 
-    @OnEvent('station.Membership.MembershipCreated')
-    onMembershipCreated(event: Event) {
-      this.addContractToGroup(event.data.membership, 'station.Membership')
-    }
+  @OnEvent("station.MembershipFactory.MembershipCreated")
+  onMembershipCreated(event: Event) {
+    this.addContractToGroup(event.data.membership, "station.Membership");
+  }
 }
 
-export default Membership
+export default Membership;
--- TokenOperator/spec.ts
+++ TokenOperator/spec.ts
@@ -5,7 +5,6 @@ import {
   Event,
   OnEvent,
   Address,
-  String,
 } from "@spec.dev/core";
 
 /**
@@ -17,15 +16,15 @@ import {
 class TokenOperator extends LiveObject {
   // The membership contract.
   @Property()
-  contractAddress: Address
+  contractAddress: Address;
 
   // The token operator address.
   @Property()
-  operatorAddress: Address
+  operatorAddress: Address;
 
   // The permissions of the token operator.
   @Property()
-  permissions: String
+  permissions: string;
 
   // ==== Event Handlers ===================
 
