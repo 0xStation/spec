ecd3b0779c2709df3a47123d85657c790801188b
Ben Whittle <benwhittle31@gmail.com>
2023-07-28 08:55:27 -0500

    db example

--- .spec/handlers/index.js
+++ .spec/handlers/index.js
@@ -6,9 +6,16 @@ const relayEventToWebhook = require('./reporter')
 async function onMembershipTransfer(event, db, logger) {    
     await relayEventToWebhook(event, logger)
 
-    // ...
-    // Anything else we wanna do that leverages the db object
-    // ...
+    /*
+        Anything else you wanna do that leverages the 'db' object:
+        -----------------------------------------------------------
+
+        // Insert example
+        await db.table("MyTable").insert({ column1: "value1", column2: "value2" })
+
+        // Update example
+        await db.table("MyTable").update({ column1: "value1" }).where({ column2: "value2" })
+    */
 }
 
 /**
