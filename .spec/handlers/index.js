7e5934df4ba2e9adb8ed546ee916f3ae4b56aaa4
Michael Gingras <mcg79@cornell.edu>
2023-08-10 10:57:45 -0600

    cleaning up handler

--- .spec/handlers/index.js
+++ .spec/handlers/index.js
@@ -1,9 +1,9 @@
 const relayEventToWebhook = require("./reporter");
 
+/**
+ * Process Station metadata queue item.
+ */
 async function processOnChainItem(event, db, logger) {
-  delete event.data.contractAddress;
-  delete event.data.transactionHash;
-
   const payload = {
     chainId: event.origin.chainId,
     transactionHash: event.origin.transactionHash,
@@ -14,22 +14,6 @@ async function processOnChainItem(event, db, logger) {
   };
 
   await relayEventToWebhook(payload, logger);
-}
-/**
- * Relay Membership Transfer events to our custom webhook.
- */
-async function onMembershipTransfer(event, db, logger) {
-  await relayEventToWebhook(event, logger);
-  /*
-        Anything else you wanna do that leverages the 'db' object:
-        -----------------------------------------------------------
-
-        // Insert example
-        await db.table("MyTable").insert({ column1: "value1", column2: "value2" })
-
-        // Update example
-        await db.table("MyTable").update({ column1: "value1" }).where({ column2: "value2" })
-    */
 }
 
 /**
