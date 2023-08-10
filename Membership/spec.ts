7bd8733318e43d2b18f30499c93bb5580d6281da
Michael Gingras <mcg79@cornell.edu>
2023-08-10 10:56:56 -0600

    removing semi-colons

--- Membership/spec.ts
+++ Membership/spec.ts
@@ -9,8 +9,8 @@ import {
   Timestamp,
   ZERO_ADDRESS,
   ERC6551Registry,
-} from "@spec.dev/core";
-import { REGISTRY_IMPLEMENTATION } from "./constants.ts";
+} from "@spec.dev/core"
+import { REGISTRY_IMPLEMENTATION } from "./constants.ts"
 
 /**
  * A Membership NFT on Station.
@@ -21,56 +21,56 @@ import { REGISTRY_IMPLEMENTATION } from "./constants.ts";
 class Membership extends LiveObject {
   // The membership contract.
   @Property()
-  contractAddress: Address;
+  contractAddress: Address
 
   // The NFT token id.
   @Property()
-  tokenId: BigInt;
+  tokenId: BigInt
 
   // The current NFT owner.
   @Property()
-  ownerAddress: Address;
+  ownerAddress: Address
 
   // The token-bound account.
   @Property()
-  tbaAddress: Address;
+  tbaAddress: Address
 
   // When the membership was minted.
   @Property({ canUpdate: false })
-  joinedAt: Timestamp;
+  joinedAt: Timestamp
 
   // ==== Event Handlers ===================
 
   @OnEvent("station.Membership.Transfer")
   async onTransfer(event: Event) {
-    this.contractAddress = event.origin.contractAddress;
-    this.tokenId = BigInt.from(event.data.tokenId);
-    this.ownerAddress = event.data.to;
+    this.contractAddress = event.origin.contractAddress
+    this.tokenId = BigInt.from(event.data.tokenId)
+    this.ownerAddress = event.data.to
 
     // Mark joinedAt and resolve TBA address from ERC6551Registry only on mints.
     if (event.data.from === ZERO_ADDRESS) {
-      this.joinedAt = this.blockTimestamp;
-      await this._resolveTbaAddress();
+      this.joinedAt = this.blockTimestamp
+      await this._resolveTbaAddress()
     }
   }
 
   @OnEvent("station.MembershipFactory.MembershipCreated")
   onMembershipCreated(event: Event) {
-    this.addContractToGroup(event.data.membership, "station.Membership");
+    this.addContractToGroup(event.data.membership, "station.Membership")
   }
 
   // ==== HELPERS ==========================
 
   async _resolveTbaAddress() {
-    const registryContract = new ERC6551Registry(this.chainId);
+    const registryContract = new ERC6551Registry(this.chainId)
     this.tbaAddress = await registryContract.account(
       REGISTRY_IMPLEMENTATION,
       this.chainId,
       this.contractAddress,
       this.tokenId,
       0
-    );
+    )
   }
 }
 
-export default Membership;
+export default Membership
