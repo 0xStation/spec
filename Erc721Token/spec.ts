b3422f9cb8c2117a3d0028a1757eff07d1234dc5
Conner Swenberg <cls364@cornell.edu>
2023-11-24 10:06:42 -0800

    Add mintedAt and stationTbaAddress to Erc721Token Live Table

--- Erc721Token/spec.ts
+++ Erc721Token/spec.ts
@@ -6,7 +6,11 @@ import {
   OnEvent,
   Address,
   BigInt,
+  Timestamp,
+  isNullAddress,
+  ERC6551Registry,
 } from "@spec.dev/core";
+import * from "./constants.ts"
 
 /**
  * An ERC721 Token on Station.
@@ -27,12 +31,35 @@ class Erc721Token extends LiveTable {
   @Property()
   ownerAddress: Address;
 
+  @Property()
+  mintedAt: Timestamp;
+
+  @Property()
+  stationTbaAddress: Address;
+
   // ==== Event Handlers ===================
   @OnEvent("station.ERC721.Transfer")
   onTransfer(event: Event) {
     this.tokenContractAddress = event.origin.contractAddress;
     this.tokenId = BigInt.from(event.data.tokenId);
     this.ownerAddress = event.data.to;
+
+    // Mark joinedAt and resolve TBA address from ERC6551Registry only on mints.
+    if (isNullAddress(event.data.from)) {
+      this.mintedAt = this.blockTimestamp
+      await this._resolveTbaAddress()
+    }
+  }
+
+  async _resolveTbaAddress() {
+    const registryContract = new ERC6551Registry(this.chainId, ERC6551_REGISTRY)
+    this.stationTbaAddress = await registryContract.account(
+      ERC6551_ACCOUNT_PROXY,
+      STATION_ACCOUNT_SALT,
+      this.chainId,
+      this.contractAddress,
+      this.tokenId
+    )
   }
 }
 
